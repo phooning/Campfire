ab5c2d384cb61be981a7262f7f01a7ef8e87e407
David Pham <davidpham@utexas.edu>
2020-10-24 00:33:22 -0500

    Add TS

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'module-resolver',
+    {
+      root: ['/src'],
+      extensions: ['.ios.js', '.android.js', '.js', '.tsx', '.json'],
+      alias: {
+        '@components': './src/components',
+        '@features': './src/features',
+        '@screens': './src/screens',
+        '@stacks': './src/stacks',
+        '@store': './src/store',
+        '@styles': './src/styles',
+        '@utils': './src/utils',
+      },
+    },
+  ],
 };
